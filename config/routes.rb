202e46119d3274ebcea49cfbc65e7eaaa9087e4d
Alexandra Chen <alechen04@gmail.com>
2016-03-02 18:00:07 -0500

    Added admin role to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root "sites#in_construction"
   get '/home' => 'sites#index'
+
+  devise_scope :user do
+    get "/admin", to: "devise/sessions#new"
+    get "/logout", to: "devise/sessions#destroy", as: :signout
+  end
+
+  get "/admin/panel"          => "sites#admin_panel"
+
 end
