a8edc2ae935b96b8d1207ea0dee8ef7b2dbd4378
Alexandra Chen <alechen04@gmail.com>
2016-05-02 10:58:01 -0500

    Can add multiple pictures to one project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update , :destroy]
   def all_projects
     @projects = Project.order("created_at DESC")
   end
@@ -10,7 +11,7 @@ class ProjectsController < ApplicationController
   def new
     if current_user.admin?
       @project = current_user.projects.new
-      @photo = Photo.new
+      @photo = @project.photos.new
     else
       flash[:notice] = "Tienes que ser un adminstrador para agregar proyectos!"
         redirect_to root_path
@@ -19,10 +20,12 @@ class ProjectsController < ApplicationController
 
   def create
     @project = current_user.projects.new(project_params)
-    @photo = Photo.new(photo_params)
     if @project.save
-      @photo.project_id = @project.id
-      @photo.save
+      if params[:images]
+        params[:images].each do |image|
+          @photo = @project.photos.create(img: image)
+        end
+      end
       redirect_to admin_panel_path, notice: "Nuevo proyecto creado exitosamente!" 
     else
       flash[:notice] = "Hubo un error al crear la proyecto."
@@ -31,32 +34,39 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
-    @photo = @project.photos.first
+    if current_user.admin?
+      @project = current_user.projects.find(params[:id])
+      @photo = @project.photos.new
+    else
+      flash[:notice] = "Tienes que ser un adminstrador para editar proyectos!"
+        redirect_to root_path
+    end
   end
 
   def update
-    @project = Project.find(params[:id])
-    @project.update_attributes(project_params)
-    @photo = @project.photos.first
-    if current_user.admin?
-      if @project.save
-        redirect_to admin_panel_path, notice: "Proyecto editada exitosamente!"
-        return
+    @project = current_user.projects.find(params[:id])
+    if @project.update_attributes(project_params)
+      if params[:images]
+        params[:images].each do |image|
+          @project.photos.create(img: image)
+        end
       end
+      redirect_to admin_panel_path, notice: "Proyecto editado exitosamente!"
+      else
+      flash[:notice] = "Hubo un problema con la edición de proyecto. Intenta nuevamente!"
+      render :edit
     end
-    flash[:notice] = "Hubo un problema con la edición de proyecto. Intenta nuevamente!"
-    render :edit
+    
   end
 
   private
   def project_params
-    params.require(:project).permit(:title, :description, :published_date, :place)
-  end
+    params.require(:project).permit(:title, :description, :published_date, :place, photos_attributes: [ :_destroy, img: [] ])
+  end 
 
   def photo_params
     #photo is inside :project
     photo_json = params[:project]
-    photo_json.require(:photo).permit(:img, :project_id)
+    params.require(:photo).permit(:img, :project_id)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,6 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :img, styles: { medium: "640x480>", thumb: "200x150>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
+  # validates_attachment_size :img, :less_than => 3.megabytes
+  validates_with AttachmentSizeValidator, attributes: :img, less_than: 1.megabytes
 end
